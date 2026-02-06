42258dbedf6e1d84098165a137daad134145fbb9
Alain Groeneweg <alain.groeneweg@proton.ch>
2026-02-06 09:51:59 +0100

    Slightly darker 400 for better accessibility on text-weak

--- colors-v2.js
+++ colors-v2.js
@@ -27,7 +27,7 @@ export const defaults = {
   // For shades (> 500): % progress from baseLightness to endL (9.5%)
   // Step 50 always = startL, step 950 always = endL (anchored, no need to specify)
   // Unspecified steps = linear interpolation between control points
-  lightnessProgression: { 100: 8, 150: 16, 200: 32 ,300:62, 400: 80, 600: 12, 700:36, 800:64, 850: 76, 900: 90 },
+  lightnessProgression: { 100: 8, 150: 16, 200: 32 ,300:62, 400: 86, 600: 12, 700:36, 800:64, 850: 76, 900: 90 },
 };
 
 // 3) Export color configurations
