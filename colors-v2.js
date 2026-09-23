a04efdc6b5deedd07b1bffc59d7f69b53a39ac11
Alain Groeneweg <alain.groeneweg@proton.ch>
2026-09-23 10:31:21 +0200

    small change in lowest shades

--- colors-v2.js
+++ colors-v2.js
@@ -27,7 +27,7 @@ export const defaults = {
   // For shades (> 500): % progress from baseLightness to endL
   // Step 25 always = startL, step 975 always = endL (anchored, no need to specify)
   // Unspecified steps = linear interpolation between control points
-  lightnessProgression: {50: 4, 150: 16, 200: 32 ,300:62, 400: 86, 600: 12, 700:36, 800:52, 950: 88},
+  lightnessProgression: {50: 4, 150: 16, 200: 32 ,300:62, 400: 86, 600: 12, 700:36, 800:52, 950: 84},
  
 };
 
@@ -142,7 +142,6 @@ export const colorConfigs = [
     baseLightness: 39.03,
     endL: 4,
     hueProgression: { 25: 0, 975: 0 },
-    lightnessProgression: { 950: 84 } // increased contrast to 975 in comparison to colors 
   },
   {
     name: "slate-500",
@@ -152,7 +151,6 @@ export const colorConfigs = [
     endL: 4,
     hueProgression: { 25: -10, 975: 0 },
     saturationProgression: { 25: 20, 200: 50, 800: 100, 975: 20 },
-    lightnessProgression: { 950: 84 } // increased contrast to 975 in comparison to colors
   },
   {
     name: "zinc-500",
@@ -162,7 +160,6 @@ export const colorConfigs = [
     endL: 4,
     hueProgression: { 25: -10, 975: 0 },
     saturationProgression: { 25: 20, 200: 50, 800: 100, 975: 20 },
-    lightnessProgression: { 950: 84 } // increased contrast to 975 in comparison to colors
   },
   {
     name: "neutral-500",
@@ -173,7 +170,6 @@ export const colorConfigs = [
     hueProgression: { 25: 0, 975: 0 },
     // Neutral: 0% of base (which is already 0%) = true grayscale (no color)
     saturationProgression: { 25: 0, 975: 0 },
-    lightnessProgression: { 950: 84 } // increased contrast to 975 in comparison to colors
   }
   
 ];
